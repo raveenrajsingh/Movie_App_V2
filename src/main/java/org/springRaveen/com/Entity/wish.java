f0cefcb5160dac41611a28813a5bc2a4f38e1558
raveenrajsingh <68072869+raveenrajsingh@users.noreply.github.com>
2021-12-05 22:15:45 +0530

    Add files via upload

---
+++ src/main/java/org/springRaveen/com/Entity/wish.java
@@ -0,0 +1,23 @@
+package org.springRaveen.com.Entity;
+
+public class wish {
+	
+
+	    public wish() {
+		
+	}
+
+		private int id;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+	 
+
+	    // standard getters and setters
+	
+}
